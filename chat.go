6c4ebdf8e001a101859b22469c849b6dd7803a2d
cxq777 <cxq777@gmail.com>
2024-09-23 11:29:25 +0800

    ToolInput

--- chat.go
+++ chat.go
@@ -239,6 +239,8 @@ type ChatCompletionRequest struct {
 	Tools        []Tool `json:"tools,omitempty"`
 	// This can be either a string or an ToolChoice object.
 	ToolChoice any `json:"tool_choice,omitempty"`
+	ToolInput  any `json:"tool_input,omitempty"`
+	//ExtraBody  any `json:"extra_body,omitempty"`
 	// Options for streaming response. Only set this when you set stream: true.
 	StreamOptions *StreamOptions `json:"stream_options,omitempty"`
 	// Disable the default behavior of parallel tool calls by setting it: false.
